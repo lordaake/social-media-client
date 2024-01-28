225bdf57e6681bbee14dd49a04f0fbfa17ff23db
lordaake <lordaake@protonmail.com>
2024-01-28 14:07:39 +0100

    timeout added

--- cypress/e2e/CA-tests/logout.spec.cy.js
+++ cypress/e2e/CA-tests/logout.spec.cy.js
@@ -25,10 +25,10 @@ describe('logout', () => {
     // Click the available logout button
     cy.get("button[data-auth='logout']:visible").click();
 
-    cy.debug();
-
     // Verify logout was successful
-    cy.get("button[data-auth='logout']:visible").should('not.exist');
+    cy.get("button[data-auth='logout']:visible").should('not.exist', {
+      timeout: 10000,
+    });
     cy.window().its('localStorage.profile').should('not.exist');
     cy.window().its('localStorage.token').should('not.exist');
   });
